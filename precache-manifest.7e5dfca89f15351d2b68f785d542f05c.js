863860f6785e757848ca9a1ae9e3af99fcf73962
Joseph Lui <joseph.lui2000@gmail.com>
2019-02-18 18:01:33 -0500

    Updates

---
+++ precache-manifest.7e5dfca89f15351d2b68f785d542f05c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c030650554fa60e1aa5",
+    "url": "/clickygame/static/css/main.eb7d4380.chunk.css"
+  },
+  {
+    "revision": "1c030650554fa60e1aa5",
+    "url": "/clickygame/static/js/main.1c030650.chunk.js"
+  },
+  {
+    "revision": "976821e7c7c83f0311bc",
+    "url": "/clickygame/static/js/runtime~main.976821e7.js"
+  },
+  {
+    "revision": "56d055b86f8a68f553b6",
+    "url": "/clickygame/static/js/2.56d055b8.chunk.js"
+  },
+  {
+    "revision": "14fc8e9a92a0dbefdd49eb1c84d8caf8",
+    "url": "/clickygame/static/media/ernie2.14fc8e9a.jpeg"
+  },
+  {
+    "revision": "9f91a6b0da6775fee7fc5e2d4658a363",
+    "url": "/clickygame/static/media/oscar2.9f91a6b0.jpg"
+  },
+  {
+    "revision": "0d79d5d57e5d614bc96b17f9fa69fe93",
+    "url": "/clickygame/static/media/team2.0d79d5d5.jpeg"
+  },
+  {
+    "revision": "8a9ba48590271be446976b705c215636",
+    "url": "/clickygame/static/media/background.8a9ba485.jpg"
+  },
+  {
+    "revision": "c2a7dd0b70d3071c7d126f8098a3d46a",
+    "url": "/clickygame/index.html"
+  }
+];
